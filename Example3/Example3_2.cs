fd950d1737c9d6ab2d94ae244700016d6488bfa8
Meown <vadimcatcat@gmail.com>
2024-10-09 14:17:10 +0200

    Example3

--- Example3/Example3_2.cs
+++ Example3/Example3_2.cs
@@ -1,19 +1,28 @@
+// Плохой нейминг, т.к. Player это не только перемещение, в данном случае это скорее Movement
 public class Player
 {
     private List<Vector2> activeWalkPath = null;
+    // isMoving не используется в коде, можно удалить
+    // Так же это поле не имеет смысла, т.к. есть activeWalkPath, которое и так показывает что игрок двигается, 
+    // можно преобразовать в свойство, которое будет возвращать true, если activeWalkPath не равен null
     private bool isMoving;
     private Vector2 currentPosition;
+    // currentEnemy private и не устанавливается(если принять что это за скобками то ок), иначе код не пройдет дальше первого if
     private Player currentEnemy;
 
     public void Update()
     {
+        // так же смешана логика поиска пути и логика следованию по пути, стоит разделить это на два класса,
+        // т.к. поиск пути может понадобиться в других сценариях
         if (currentEnemy != null)
         {
             var builtPath = TryBuildPathToCoord(currentEnemy.currentPosition);
             if (builtPath == null)
             {
+                // Если мы убираем врага если путь не найден, то персонаж опять же замирает на месте, поиск пути вмешивается в выбор цели
                 currentEnemy = null;
                 isMoving = false;
+                // если мы не меняем сигнаутру, то в обоих случаях activeWalkPath = builtPath; будет выполнять, то что делает код сейчас
                 activeWalkPath = null;
             }
             else
@@ -24,7 +33,9 @@ public class Player
         }
     }
 
-
+    // Из использование в коде видно что, метод может вернуть null, если путь не удалось построить, что явлется плохой практикой,
+    // стоит переписать метод так, чтобы он возвращал пустой список, если путь не удалось построить или изменить под сигнатуру 
+    // bool TryBuildPathToCoord(Vector2 target, out List<Vector2> path)
     private List<Vector2> TryBuildPathToCoord(Vector2 target)
     {
         return <много строк кода по созданию пути из currentPosition в target>;
